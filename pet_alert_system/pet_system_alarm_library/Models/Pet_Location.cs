210e859d5223438a9401ad5cc321872797871d1f
diegoares02@gmail.com <diegoares02@gmail.com>
2021-02-10 21:22:46 -0400

    Models added to the library

---
+++ pet_alert_system/pet_system_alarm_library/Models/Pet_Location.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pet_system_alarm_library.Models
+{
+    public class Pet_Location
+    {
+        public int Id { get; set; }
+        public int IdPet { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public double Speed { get; set; }
+        private DateTime locationDate;
+
+        public DateTime LocationDate
+        {
+            get { return DateTime.Now; }
+            set { locationDate = value; }
+        }
+
+    }
+}
---
+++ pet_alert_system/pet_system_alarm_library/Models/Veterinary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pet_system_alarm_library.Models
+{
+    public class Veterinary
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Schedule { get; set; }
+        public string Address { get; set; }
+    }
+}
